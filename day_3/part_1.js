f7c566f2b31c33eccb2f583049468c211553def3
Colton Palmer <colton@coltonpalmer.com>
2023-12-13 17:43:40 -0500

    improved day 3 solutions

--- day_3/part_1.js
+++ day_3/part_1.js
@@ -3,78 +3,45 @@ const util = require('util');
 
 const input = fs.readFileSync('input.txt', 'utf-8');
 
-const findNumber = (numbers, row, col) => {
-    let res;
-    numbers.forEach((number) => {
-        if (number.connected) return;
+const findAdjacentNumbers = (specialChar) => {
+    const adjacentNumbers = [];
+    
+    for (let row = specialChar.row - 1; row <= specialChar.row + 1; row++) {
+        for (let col = specialChar.col - 1; col <= specialChar.col + 1; col++) {
+            if (!/\d/.test(grid[row][col])) continue;
+            
+            for (const number of numbers) {
+                if ('adjacent' in number) continue;
+                if (number.row != row) continue;
+                if (col < number.col || col > number.col + number.value.length - 1) continue;
 
-        if (number.position.row != row) return;
-
-        if (col < number.position.col || (number.position.col + number.length) < col ) return;
-
-        number.connected = true;
-
-        res = number;
-    });
-
-    return res;
-}
-
-const tokenizeLine = (line, lineNum) => {
-    const tokens = [];
-
-    let match;
-
-    // tokenize numbers
-    let index = 0;
-    while ((match = line.substring(index).match(/(?<!\d)\d+/)) != null) {
-        tokens.push({
-            type: 'number',
-            value: parseInt(match[0]),
-            position: { row: lineNum, col: index + match.index },
-            length: match[0].length,
-            connected: false
-        });
-
-        index += match['index'] + match[0].length;
+                adjacentNumbers.push(number);
+                number.adjacent = true;
+            }
+        }
     }
 
-    // tokenize special characters
-    index = 0;
-    while ((match = line.substring(index).match(/[^\d\.]/)) != null) {
+    specialChar.adjacentNumbers = adjacentNumbers;
+    return specialChar;
+}
 
-        tokens.push({
-            type: 'special',
-            value: match[0],
-            position: { row: lineNum, col: index + match.index }
-        });
+const tokenizeLine = (line, lineNr) => {
+    const tokens = [];
 
-        index += match['index'] + match[0].length;
+    for (const match of line.matchAll(/\d+|[^\d\.]/g)) {
+        const token = { value: match[0], row: lineNr, col: match.index };
+        
+        token.type = /\d/.test(match[0]) ? 'number' : 'special';
+
+        tokens.push(token);
     }
 
     return tokens;
 }
 
-const connectSpecialCharsToNums = (specialChar) => {
-        // search adjacent squares for numbers
-        specialChar.numbers = [];
-        for (let row = specialChar.position.row - 1; row <= specialChar.position.row + 1; row++) {
-            for (let col = specialChar.position.col - 1; col <= specialChar.position.col + 1; col++) {
-                if (/\d/.test(grid[row][col])) {
-                    // attach corresponding number to special character if it exists
-                    let number = findNumber(numbers, row, col);
-
-                    if (number != null)
-                        specialChar.numbers.push(number);
-                }
-            }
-        }
-
-        return specialChar;
-    }
-
-
-const grid = input.split('\n');
+const grid = input
+    .split('\n')
+    .filter(Boolean);
 
 const tokens = grid
     .map(tokenizeLine)
@@ -85,10 +52,10 @@ const specialChars = tokens
 
 const numbers = tokens
     .filter((token) => token.type == 'number');
-    
+
 const res = specialChars
-    .map(connectSpecialCharsToNums)
-    .map((specialChar) => specialChar.numbers.reduce((acc, number) => acc + number.value, 0))
-    .reduce((acc, num) => acc + num, 0);
+    .map(findAdjacentNumbers)
+    .map((specialChar) => specialChar.adjacentNumbers.reduce((acc, number) => acc + parseInt(number.value), 0))
+    .reduce((acc, sums) => acc + sums, 0);
 
 console.log(util.inspect(res, false, null, true));
